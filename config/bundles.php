27ff0426b4f209c9343915d186d14aade45d4b56
Vladimir Turnaev <turnaev@gmail.com>
2018-09-24 17:15:19 +0700

    ~

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-
-    \Tvi\MonitorBundle\TviMonitorBundle::class => ['dev' => true],
+    Tvi\MonitorBundle\TviMonitorBundle::class => ['dev' => true],
 ];
