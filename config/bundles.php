a371a8c83b637ba81a99bd14ea3a8c5e28f99993
Vladimir Turnaev <turnaev@gmail.com>
2018-10-24 10:03:40 +0700

    ~

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+
     Tvi\MonitorBundle\TviMonitorBundle::class => ['all' => true],
 ];
